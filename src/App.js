0f2dbfcd71b852e0faf0d1df740ea5254ffdd23f
sevenaper <lxsdwlhh@gmail.com>
2018-11-30 19:59:34 +0800

    框架差不多好了

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MyHeader from './common/header';
+import Home from './pages/home';
+import Detail from './pages/detail/loadable.js';
+import Login from './pages/login';
+import Write from './pages/write';
+import store from './store';
+
+class App extends Component {
+  render() {
+    return (
+    	<Provider store={store}>
+      	<BrowserRouter>
+      		<div>
+            <MyHeader />
+      			<Route path='/' exact component={Home}/>
+            <Route path='/login' exact component={Login}/>
+            <Route path='/write' exact component={Write}/>
+      			<Route path='/detail/:id' exact component={Detail}/>
+      		</div>
+      	</BrowserRouter>
+      </Provider>
+    );
+  }
+}
+
+export default App;
